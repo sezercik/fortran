9343471d90ffa92e9fd4024c3418c940c717e2c6
sezercik <szrakoraler@gmail.com>
2023-06-04 15:49:22 +0300

    real

--- integers.f90
+++ integers.f90
@@ -2,14 +2,18 @@ program integers
 
     implicit none
 
+    !integer is normal integer
     integer(kind = 2) :: shortval
     integer(kind = 4) :: longval
 
+    !real is for float values
+    real :: floatval
     ! you can specify bytes by using kind
 
 
     ! huge function gives  maximum value that variable can hold
     print *, huge(shortval)
+    print *, huge(floatval)
 
 
 end program integers
