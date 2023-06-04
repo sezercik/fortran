a7d9ecb33107aa88e4e05bfcdb976a25271105ac
sezercik <szrakoraler@gmail.com>
2023-06-04 04:38:43 +0300

    integers

---
+++ integers.f90
@@ -0,0 +1,15 @@
+program integers
+
+    implicit none
+
+    integer(kind = 2) :: shortval
+    integer(kind = 4) :: longval
+
+    ! you can specify bytes by using kind
+
+
+    ! huge function gives  maximum value that variable can hold
+    print *, huge(shortval)
+
+
+end program integers
