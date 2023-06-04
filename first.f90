bf1cfef3fc54385a27b7709780926f2351b3cd80
sezercik <szrakoraler@gmail.com>
2023-06-04 04:20:26 +0300

    first commit

---
+++ first.f90
@@ -0,0 +1,15 @@
+program first
+
+    ! implicit none mean deacleare all variables i think
+    implicit none
+
+    real :: a,b, result
+
+    a = 12
+    b = 15
+
+    result = a + b
+    print*, 'total sum is ', result
+    print*, "fortran seems good"
+
+end program first
